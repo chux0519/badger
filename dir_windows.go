fc4974b207a0a732f93b2a79d9b274fcd5c9b41d
Yongsheng Xu <chuxdesign@hotmail>
2021-09-07 13:01:01 +0800

    Revert "win: add syncDir back to avoid panic after power loss corruption"
    This reverts commit 2b372d61584c132536722255bc8d3371d78bf909.

--- dir_windows.go
+++ dir_windows.go
@@ -105,18 +105,6 @@ func (g *directoryLockGuard) release() error {
 	return syscall.CloseHandle(g.h)
 }
 
-// Windows doesn't support syncing directories through network(like SMB). See
+// Windows doesn't support syncing directories to the file system. See
 // https://github.com/dgraph-io/badger/issues/699#issuecomment-504133587 for more details.
-func syncDir(dir string) error {
-	f, err := openDir(dir)
-	if err != nil {
-		return y.Wrapf(err, "While opening directory: %s.", dir)
-	}
-
-	err = f.Sync()
-	closeErr := f.Close()
-	if err != nil {
-		return y.Wrapf(err, "While syncing directory: %s.", dir)
-	}
-	return y.Wrapf(closeErr, "While closing directory: %s.", dir)
-}
+func syncDir(dir string) error { return nil }
